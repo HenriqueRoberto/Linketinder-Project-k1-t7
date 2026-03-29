4ffdd6603934b574706b34e386a42358a21b7f70
Henrique Roberto dos Santos <contatohenrique.hrs@gmail.com>
2026-03-29 14:15:45 -0300

    feature-vaga[model\empresa]:[feat]lista de vagas e proximo id

--- src/main/groovy/linketinder/model/Empresa.groovy
+++ src/main/groovy/linketinder/model/Empresa.groovy
@@ -1,7 +1,15 @@
 package linketinder.model
 
 class Empresa extends PessoaJuridica {
+
+    // Contador estático para gerar ids únicos e sequenciais
+    private static int proximoId = 1
+
+    int id
+    List<Vaga> vagas = []
+
     Empresa(String nome, String email, String cnpj, String pais, String estado, String cep, String descricao, List<String> competencias, String senha) {
         super(nome, email, cnpj, pais, estado, cep, descricao, competencias, senha)
+        this.id = proximoId++
     }
 }
